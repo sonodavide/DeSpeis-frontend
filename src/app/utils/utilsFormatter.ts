c7e09a3c4a9fcffec037c07e58cd8f5060761abb
sonodavide <26660577+sonodavide@users.noreply.github.com>
2025-06-15 02:14:51 +0200

    fixed ordini to string

--- src/app/utils/utilsFormatter.ts
+++ src/app/utils/utilsFormatter.ts
@@ -2,7 +2,7 @@ import { BigliettoDto } from "../model/bigliettoDto";
 
 export class UtilsFormatter {
     static bigliettoToString(biglietto : BigliettoDto) : string{
-        return `${biglietto.id} - ${biglietto.postospettacoloPosto} - ${biglietto.postospettacoloSpettacoloData} - ${biglietto.postospettacoloSpettacoloFilmTitolo} - ${biglietto.prezzo}`
+        return `${biglietto.id} - [${biglietto.postospettacoloPosto.fila} - ${biglietto.postospettacoloPosto.sedile }] - ${biglietto.postospettacoloSpettacoloData} - ${biglietto.postospettacoloSpettacoloFilmTitolo} - ${biglietto.prezzo}`
     }
     static formatTime(time: string): string {
         return time.substring(0, 5); // Mostra solo ore e minuti (HH:mm)
